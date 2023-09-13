c21baeb332a664d857385a6d01d54c5e97dc5bb3
Ilikeshoes21 <143002406+Ilikeshoes21@users.noreply.github.com>
2023-09-13 14:19:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Jocelyn Uribe #Naomi Garcia
+name = input("What is your name?")
+sold = input("How much have you sold this month?")
+float_comm = float(sold)
+results = float_comm * 13 / 100
+print(round(results, 2))
+print(f'Hello {name}, you have earned ${results} this month.')
